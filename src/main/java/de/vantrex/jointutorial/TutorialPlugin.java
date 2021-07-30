624b06e3e82b28c3986d734fa35374f034e8c8b4
Marinus Vantrex <vantrexplays@gmail.com>
2021-07-30 21:30:01 +0200

    Initial Commit

--- src/main/java/de/vantrex/jointutorial/TutorialPlugin.java
+++ src/main/java/de/vantrex/jointutorial/TutorialPlugin.java
@@ -26,6 +26,7 @@ public class TutorialPlugin extends JavaPlugin {
         getCommand("tutorialset").setExecutor(new LocationCreateCommand());
         getCommand("tutorialaddmsg").setExecutor(new AddMessageCommand());
         getCommand("tutorialdelete").setExecutor(new DeleteLocationCommand());
+        //
     }
 
     @Override
